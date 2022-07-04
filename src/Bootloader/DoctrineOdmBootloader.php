ab5681c67aa3ec2c3c19296ea2b0a443bdbae644
Igor <8082887@gmail.com>
2022-07-04 17:31:49 +0600

    Fix creator naming

--- src/Bootloader/DoctrineOdmBootloader.php
+++ src/Bootloader/DoctrineOdmBootloader.php
@@ -6,8 +6,6 @@ namespace Mitrii\Spiral\Doctrine\ODM\Bootloader;
 
 use Doctrine\ODM\MongoDB\Configuration;
 use Doctrine\ODM\MongoDB\DocumentManager;
-use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
-use Doctrine\ODM\MongoDB\Mapping\Driver\AttributeDriver;
 use Doctrine\ODM\MongoDB\Tools\Console\Helper\DocumentManagerHelper;
 use Mitrii\Spiral\Doctrine\ODM\Config\DoctrineOdmConfig;
 use MongoDB\Client;
